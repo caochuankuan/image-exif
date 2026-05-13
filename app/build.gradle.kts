4780db57e930c330bfabcf672f629886271a7b51
chuankuan cao <917127374@qq.com>
2026-05-13 12:57:36 +0800

    feat: 更新版本信息

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,15 @@ plugins {
 android {
     namespace = "com.compose.exifinfo"
     compileSdk {
-        version = release(36)
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.compose.exifinfo"
         minSdk = 25
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 37
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
